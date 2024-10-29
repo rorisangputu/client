7e8a19789d0ea4990b87908dcd930795239f2dd2
Rorisang Putu <rorisangputu@gmail.com>
2024-10-29 21:33:41 +0200

    adding toast provider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
 import { auth } from "@clerk/nextjs/server";
 import Footer from "@/components/Footer";
+import ToastProvider from "@/providers/ToastProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,12 +36,14 @@ async function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ToastProvider />
           <img
             src="/img/hero.svg"
             className="absolute -z-10 top-0 right-0 w-full md:w-[60%]"
             alt=""
           />
           <Header userId={userId} />
+
           {children}
           <Footer />
         </body>
